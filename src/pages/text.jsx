8d34711632afa1aec61461949d6326b6fe4dba4e
aminigbo <aminigbopaul@gmail.com>
2025-06-17 16:23:07 +0100

    New static page

--- src/pages/text.jsx
+++ src/pages/text.jsx
@@ -6,13 +6,9 @@ const MyPage = () => {
             <Helmet>
                 <title>My Page Title</title>
                 <meta property="og:title" content="My Page Title" />
-                <meta property="og:description" content="Page description here" />
-                <meta property="og:image" content="https://tcijrogmncatfkddtyzm.supabase.co/storage/v1/object/public/book/0.9235983413282883.jpg" />
-                <meta property="og:type" content="website" />
+                <link rel="apple-touch-icon" href="https://tcijrogmncatfkddtyzm.supabase.co/storage/v1/object/public/book/0.9235983413282883.jpg" />
+                <link rel="icon" href="https://tcijrogmncatfkddtyzm.supabase.co/storage/v1/object/public/book/0.9235983413282883.jpg" />
 
-                <meta name="twitter:card" content="summary_large_image" />
-                <meta name="twitter:title" content="My Page Title" />
-                <meta name="twitter:description" content="Page description here" />
                 <meta name="twitter:image" content="https://tcijrogmncatfkddtyzm.supabase.co/storage/v1/object/public/book/0.9235983413282883.jpg" />
             </Helmet>
 
